e3cf78546c666cf79edfb0e44de4dff4ea31a9b1
ZenkiOo <zenky@list.ru>
2022-05-04 19:57:05 +0300

    add some style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import { SomeList } from './pages/SomeList';
 import { HomePage } from './pages/HomePage';
+
 import './App.css';
+import './css/reset.css';
+import './css/header.css';
 
 function App() {
   return (
     <>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/some-list'}>SomeList</Link>
+      <div className='nav-links'>
+        <Link className='nav-links__link' to={'/'}>
+          Home
+        </Link>
+        <Link className='nav-links__link' to={'/some-list'}>
+          SomeList
+        </Link>
+      </div>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/some-list' element={<SomeList />} />
