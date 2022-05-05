90e66fad841895c069b522dd9ebc96815d62c345
ZenkiOo <zenky@list.ru>
2022-05-05 13:55:48 +0300

    add some style

--- src/pages/SomeList.js
+++ src/pages/SomeList.js
@@ -1,6 +1,8 @@
 import { List } from '../components/List';
+import '../css/some-list.css';
+
 const SomeList = () => {
-  const items = ['1', '2'];
+  const items = ['1', '2', '3', '4'];
   return <section className='items'>{<List items={items} />}</section>;
 };
 export { SomeList };
